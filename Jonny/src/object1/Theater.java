d507c54ada5e1c93ffa60556c39fffac2f977c3c
Jonny-Cho <andante2183@gmail.com>
2019-08-30 13:33:26 +0900

    Object1 version2 Increase autonomy of ticketSeller p.20

--- Jonny/src/object1/Theater.java
+++ Jonny/src/object1/Theater.java
@@ -8,20 +8,6 @@ public class Theater {
 	}
 	
 	public boolean enter(Audience audience) {
-		if (audience.getBag().hasInvitation()) {
-			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-			audience.getBag().setTicket(ticket);
-		} else {
-			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-			if(audience.getBag().getAmount() >= ticket.getFee()) {
-				audience.getBag().minusAmount(ticket.getFee());
-				ticketSeller.getTicketOffice().plusAMount(ticket.getFee());
-				audience.getBag().setTicket(ticket);
-			} else {
-				ticketSeller.getTicketOffice().setTicket(ticket);
-			}
-		}
-		
-		return audience.getBag().hasTicket();
+		return ticketSeller.sellTo(audience);
 	}
 }
--- Jonny/src/object1/TicketSeller.java
+++ Jonny/src/object1/TicketSeller.java
@@ -7,7 +7,25 @@ public class TicketSeller {
 		this.ticketOffice = ticketOffice;
 	}
 	
-	public TicketOffice getTicketOffice() {
-		return ticketOffice;
+//	public TicketOffice getTicketOffice() {
+//		return ticketOffice;
+//	}
+	
+	public boolean sellTo(Audience audience) {
+		if (audience.getBag().hasInvitation()) {
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().setTicket(ticket);
+		} else {
+			Ticket ticket = ticketOffice.getTicket();
+			if(audience.getBag().getAmount() >= ticket.getFee()) {
+				audience.getBag().minusAmount(ticket.getFee());
+				ticketOffice.plusAMount(ticket.getFee());
+				audience.getBag().setTicket(ticket);
+			} else {
+				ticketOffice.setTicket(ticket);
+			}
+		}
+		
+		return audience.getBag().hasTicket();
 	}
 }
