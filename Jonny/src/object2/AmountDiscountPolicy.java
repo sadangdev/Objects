dac9fbbfe5edc70e3f739e1de7e3c4a248d1c697
Jonny-Cho <andante2183@gmail.com>
2019-09-04 15:46:33 +0900

    Add object2

---
+++ Jonny/src/object2/AmountDiscountPolicy.java
@@ -0,0 +1,21 @@
+package object2;
+
+public class AmountDiscountPolicy extends DiscountPolicy {
+	private Money discountAmount;
+
+	public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+		super(conditions);
+		this.discountAmount = discountAmount;
+	}
+
+	@Override
+	protected Money getDiscountAmount(Screening screening) {
+		return discountAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "AmountDiscountPolicy [discountAmount=" + discountAmount + "]";
+	}
+	
+}
---
+++ Jonny/src/object2/PercentDiscountPolicy.java
@@ -0,0 +1,21 @@
+package object2;
+
+public class PercentDiscountPolicy extends DiscountPolicy {
+	private double percent;
+	
+	public PercentDiscountPolicy(double percent, DiscountCondition... conditions) {
+		super(conditions);
+		this.percent = percent;
+	}
+
+	@Override
+	protected Money getDiscountAmount(Screening screening) {
+		return screening.getMovieFee().times(percent);
+	}
+
+	@Override
+	public String toString() {
+		return "PercentDiscountPolicy [percent=" + percent + "]";
+	}
+	
+}
