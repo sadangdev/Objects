5812f0ba7094b521f452ca6fd1a202b49274d6aa
Jonny-Cho <andante2183@gmail.com>
2019-09-06 15:37:12 +0900

    Add README.md in jonny directory

--- Jonny/src/object4/Main.java
+++ Jonny/src/object4/Main.java
@@ -60,8 +60,6 @@ public class Main {
 		System.out.println(reservation);
 		System.out.println(reservation.getFee());
 		
-//		System.out.println(titanic.toString());
-//		System.out.println(starwars.toString());
 	}
 
 }
